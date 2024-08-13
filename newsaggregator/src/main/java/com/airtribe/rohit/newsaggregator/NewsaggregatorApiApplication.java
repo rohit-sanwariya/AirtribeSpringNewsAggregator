f38a6e1c0c293b8df9b44bfdeaf8f4436266b186
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2024-08-13 13:05:53 +0530

    spring init setup

---
+++ newsaggregator/src/main/java/com/airtribe/rohit/newsaggregator/NewsaggregatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.airtribe.rohit.newsaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsaggregatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsaggregatorApiApplication.class, args);
+	}
+
+}
---
+++ newsaggregator/src/test/java/com/airtribe/rohit/newsaggregator/NewsaggregatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtribe.rohit.newsaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsaggregatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
