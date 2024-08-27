45213c38bb43f6a84443c69dca475615d9cd4958
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2024-08-27 11:30:10 +0530

    logout on 403 with http interceptor

--- newsaggregator-angular-client/src/app/app.config.ts
+++ newsaggregator-angular-client/src/app/app.config.ts
@@ -3,12 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { newsHttpInterceptor } from './news-http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true })
     , provideRouter(routes),
     provideAnimationsAsync(),
-  provideHttpClient()]
+  provideHttpClient(
+    withInterceptors([newsHttpInterceptor])
+  )]
 };
---
+++ newsaggregator-angular-client/src/app/news-http.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+export const newsHttpInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router)
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 403) {
+        localStorage.clear();
+        router.navigate(['/login']); // Redirect to login page
+      }
+      return throwError(()=>error);
+    })
+  );
+};
